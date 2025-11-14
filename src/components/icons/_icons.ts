fd6db0d11dd32acdc24cb24559b712b7ab6642bd
bastndev <gohit21@gmail.com>
2025-11-13 23:46:06 -0500

    🏗️ [Add] social media icons and update asset files rectory.

--- src/components/icons/_icons.ts
+++
@@ -1,8 +0,0 @@
-export { FacebookIcon } from './facebook';
-export { GitHubIcon } from './github';
-export { InstagramIcon } from './instagram';
-export { LinkedInIcon } from './linkedin';
-export { SoundCloudIcon } from './soundcloud';
-export { TikTokIcon } from './tiktok';
-export { TwitterIcon } from './twitter';
-export { YouTubeIcon } from './youtube';
---
+++ src/components/icons/icons.ts
@@ -0,0 +1,8 @@
+export { FacebookIcon } from './social-media/facebook';
+export { GitHubIcon } from './social-media/github';
+export { InstagramIcon } from './social-media/instagram';
+export { LinkedInIcon } from './social-media/linkedin';
+export { SoundCloudIcon } from './social-media/soundcloud';
+export { TikTokIcon } from './social-media/tiktok';
+export { TwitterIcon } from './social-media/twitter';
+export { YouTubeIcon } from './social-media/youtube';
