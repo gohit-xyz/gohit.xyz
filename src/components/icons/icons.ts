03e66a1297ac5b9caf71a4121d8d330affe7e789
bastndev <gohit21@gmail.com>
2025-11-20 12:24:17 -0500

    🏗️ [add] CircleBtn UI component and export ArrowUpIcon

--- src/components/icons/icons.ts
+++ src/components/icons/icons.ts
@@ -2,6 +2,7 @@
 export { ArrowBackIcon } from './global/arrow-back'
 export { ArrowLeftIcon } from './global/arrow-left'
 export { ArrowRightIcon } from './global/arrow-right'
+export { ArrowUpIcon } from './global/arrow-up'
 export { ArrowDownIcon } from './global/arrow-down'
 export { ExternalLinkIcon } from './global/external-link'
 export { SunIcon } from './global/sun'
