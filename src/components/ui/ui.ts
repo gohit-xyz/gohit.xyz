884299e74fb2980e23993df45d3b78e0dc5e6a21
bastndev <gohit21@gmail.com>
2025-11-20 17:26:07 -0500

    🏗️ [add] new button, effect, and grid components, and re-categorize an existing grid component.

--- src/components/ui/ui.ts
+++ src/components/ui/ui.ts
@@ -1,8 +1,18 @@
 // Buttons
-export { default as ExtensionsGrid } from './grids/ExtensionsGrid.astro';
-export { default as GithubStartBtn } from './buttons/GithubStartBtn.astro'
-export { default as LinuxBtn } from './buttons/LinuxBtn.astro'
-export { default as YoutubeBtn } from './buttons/YoutubeBtn.astro'
+export { default as ArrowUpBtn } from './buttons/ArrowUpBtn.astro';
+export { default as BackButtonOne } from './buttons/BackButtonOne.astro';
+export { default as BackButtonTwo } from './buttons/BackButtonTwo.astro';
+export { default as CircleBtn } from './buttons/CircleBtn.astro';
+export { default as GithubStartBtn } from './buttons/GithubStartBtn.astro';
+export { default as LinuxBtn } from './buttons/LinuxBtn.astro';
+export { default as NextPageBtn } from './buttons/NextPageBtn.astro';
+export { default as OrangeBtn } from './buttons/OrangeBtn.astro';
+export { default as YoutubeBtn } from './buttons/YoutubeBtn.astro';
 
 // Effects
-export { default as GlowEffect } from './effects/GlowEffect.astro'
+export { default as Decorative } from './effects/Decorative.astro';
+export { default as GlowEffect } from './effects/GlowEffect.astro';
+export { default as PartnersCarousel } from './effects/PartnersCarousel.astro';
+
+// Grids
+export { default as ExtensionsGrid } from './grids/ExtensionsGrid.astro';
