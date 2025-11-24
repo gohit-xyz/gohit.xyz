2e39c08f65bdaf0ea460267edb528f16de2dd672
bastndev <gohit21@gmail.com>
2025-11-24 18:13:23 -0500

    chore: update Lams sponsor photo URL and remove tier

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -14,8 +14,7 @@ export const sponsors: Sponsor[] = [
   },
   {
     name: 'Lams',
-    photo: 'https://scontent.flim6-3.fna.fbcdn.net/v/t39.30808-6/481282983_9189080574515424_1398618467427285229_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFUksblZRkltieAhESwk1306tt_GKFYjxLq238YoViPEmSgMdwGz6skLdF98Mo0E2qhWvBuS3ORZ4EJQXvc2h4S&_nc_ohc=tWpteOmzzxcQ7kNvwFoxtAx&_nc_oc=AdkBmmta6VmrEEciCspHgdWSyqG4-oGDz39qtm33s-MRLP_-KMeP4Mi7hNGKhMw_w3Q&_nc_zt=23&_nc_ht=scontent.flim6-3.fna&_nc_gid=Cu9R3GWyxzc9BQ_X_-fwuA&oh=00_AfhenU4JidWSe_EiscF-QtVbKcVIJcS7Y0tksFRlicDJyA&oe=6923FB1E',
-    tier: 'bronze',
+    photo: 'https://scontent.flim6-3.fna.fbcdn.net/v/t39.30808-6/481282983_9189080574515424_1398618467427285229_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEvcLyu2NK1tjRPyxxZxfT46tt_GKFYjxLq238YoViPEgjorz3raBJ8ZHr9HKlCYfNj1zT2RLAvoVQVCxgZuzri&_nc_ohc=21sD9I6b7RkQ7kNvwHcMOAf&_nc_oc=AdnUj0gKVw0OswV_KO_ep4sbc687V6BHDMPJFh8FH9QfyvUwuyKx1tAxYGCWcMZQThU&_nc_zt=23&_nc_ht=scontent.flim6-3.fna&_nc_gid=Fln491Elkad5NJyFkBjE5Q&oh=00_Afi3Wmg-RSnmAfgfTu_OIri9Ji49Bk0wJaSSUPo3wfJLXw&oe=692ACADE',
     // website: 'https://example.com'
   },
 ]
