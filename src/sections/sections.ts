c033041f7f4c93d787db76aa5fea896a61f65d1f
bastndev <gohit21@gmail.com>
2025-11-25 11:18:14 -0500

    🏗️ [refactor] remove Header component and add Footer export

--- src/sections/sections.ts
+++ src/sections/sections.ts
@@ -4,4 +4,5 @@ export { default as Projects } from './home/Projects.astro'
 export { default as SocialLinks } from './home/SocialLinks.astro'
 export { default as YouTubeVideo } from './home/YouTubeVideo.astro'
 
-// --- Linux
+// --- others
+export { default as Footer } from './shared/Footer.astro'
